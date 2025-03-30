57dc311ff15356deed3413301c190c7052eeb5f1
Rupam Biswas <rupam.biswas@hometriangle.com>
2025-03-30 20:12:22 +0530

    Admin login feature added!

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv'
+import 'dotenv/config'
 import connectDB from './config/mongodb.js';
-
-dotenv.config();
-console.log("MONGODB_URI:", process.env.MONGODB_URI);
-
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 //app config
 const app = express()
 const port = process.env.PORT || 4000
-// connectDB()
+connectDB()
+connectCloudinary()
 
 //middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoints
+app.use('/api/admin',adminRouter)
+
 app.get('/', (req, res)=>{
     res.send('API working')
 })
